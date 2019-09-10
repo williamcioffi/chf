5e23437ade84de2cbd2928529f19344b70cef272
williamcioffi <william.cioffi@gmail.com>
2019-09-10 06:36:45 -0700

    initial upload

---
+++ R/matchtimes.R
@@ -0,0 +1,19 @@
+#' closest match for instance for times
+#' 
+#' @param t1 a numeric vector to match
+#' @param t2 the second numeric vectgor to match
+#' @return another numeric vector of length equal to \code{t1}
+#' @details it rounds up, who cares?
+#' @export
+#' @examples
+#' # consider two vectors
+#' v1 <- seq(1, 11, by = 1)
+#' v2 <- seq(1, 11, by = 4)
+#'
+#' # matchitup
+#' matchtimes(v1, v2)
+
+matchtimes <- function(t1, t2) {
+# t1, t2 are numeric
+	findInterval(t1, c(-Inf, head(t2, -1)) + c(0, diff(t2)/2))
+}
