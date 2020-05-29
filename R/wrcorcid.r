529a508a571c6a9e4d85f13ba041a37bf4a3a3e0
williamcioffi <william.cioffi@gmail.com>
2020-05-28 21:18:13 -0400

    add in my orcid function for setting seeds

---
+++ R/wrcorcid.r
@@ -0,0 +1,11 @@
+#' my orcid id e.g. for setting seeds
+#'
+#' @return numeric representation of my orcid
+#' @export
+#' @examples
+#' \dontrun{
+#' set.seed(wrcorcid())
+#' x1 <- rnorm(100)
+#' }
+
+wrcorcid <- function() 0000000311828578
