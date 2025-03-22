0d67c7a0ab97856a40ba09432a9bbf7a89ee1c32
williamcioffi <william.cioffi@gmail.com>
2025-03-21 21:53:16 -0500

    add function for changing alpha of colors

---
+++ R/change_alpha.R
@@ -0,0 +1,31 @@
+#' change (or add) alpha to colors
+#'
+#' @param col color or vector of colors
+#' @param alpha value to add or change. on [0, 1].
+#' @return rgb colors or vector of rgbcolors with alpha
+#' @export
+#' @importFrom grDevices col2rgb rgb
+#' @examples
+#' # numeric
+#' change_alpha(1, .5)
+#' 
+#' # rgb
+#' change_alpha(rgb(1, 0, 1), .5)
+#' change_alpha("#ff00ff", .5)
+#' 
+#' # multiple
+#' change_alpha(1:3, alpha = 0.5)
+#' change_alpha(1:3, alpha = c(0.1, 0.2, 0.3))
+
+change_alpha <- function(col, alpha) {
+  if(missing(col) | missing(alpha)) {
+    stop("change_alpha: must specify at least a color and a desired alpha level")
+  }
+  
+  if(length(alpha) %% length(col) != 0 & length(alpha) != 1) {
+    stop("change_alpha: alpha must be a multiple of the number of colors")
+  }
+  
+  color <- col2rgb(col, alpha = FALSE)/255
+  rgb(t(color), alpha = alpha)
+}
